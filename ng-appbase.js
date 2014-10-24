d91b9f48dc20d09c80220a515bc7e80030a0a9ba
unknown <sagar.chandarana123@gmail.com>
2014-10-24 18:32:50 -0200

    complete recode of ng-appbase for 2.0, injects methods in appbase refrences

--- ng-appbase.js
+++ ng-appbase.js
@@ -1,271 +1,324 @@
 /**
  * Created by Sagar on 12/8/14.
  */
-angular.module('ngAppbase',[])
-  .factory('$appbaseRef',function($timeout) {
-    var ngAppbaseRef = function(input) {
-      /* accepts a path, or an Appbase reference and returns ngAppbaseRef,
-       a wrapper that supports $bindProperties and $bindEdges, which keeps updating a scope variable whenever the data is changed in Appbase.
-       'bindProperties' binds a scope variable to properties of a vertex (a JSON object)
-       'bindEdges' binds a scope variable to edges of a vertex, (an array), where edges are sorted by priorities. 'bindEdges' also returns an array object, which need not to be attached with a scope variable. Each object in array: {name: 'edgeName', priority:'edge priority', properties: {out vertex's properties}}
-       */
-      var ref;
-      if(typeof input === "string")
-        ref = Appbase.ref(input)
-      else if(typeof input === "object" && typeof input.path() === "string")
-        ref = input
-      else throw ("Invalid argument to create an ngAppbaseRef")
+(function() {
 
-      var onPropsUnbind
-      var props
-      var remoteScopeProps
-      var bindProperties = function(remoteScope, varName, callbacks) {
-        if(typeof varName === "object") {
-          callbacks = varName || {}
-          varName = undefined
+var replaceObjInPlace = function(src, dest) {
+  for(var prop in src) {
+    delete src[prop]
+  }
+
+  for(var prop in dest) {
+    src[prop] = dest[prop]
+  }
+}
+
+var parsePath = function(path) {
+  var slashIndex;
+  return {
+    ns: path.slice(0, ((slashIndex = path.indexOf('/')) !== -1) ? slashIndex : undefined),
+    v: path.slice(path.indexOf('/') + 1)
+  }
+}
+
+/* phaadArray
+   - implements sortedSet on an Array, as angular requires an array for ng-repeat
+   - add/remove O(n), can be improved using binary search
+   - automatically fetches vertices' properties and adds into the object
+   - allows onAdd/onRemove/onChange callbacks.
+*/
+var phaadArray = function(reverse, callbacks, $timeout, remoteScope) {
+  var exports = {};
+  var toBeDeleted = {};
+  exports.data = [];
+
+  handleAdd = function(name, vObj, addNow) {
+    addNow = $timeout.bind(null, addNow);
+    var updateScope = $timeout.bind(null, function() {});
+    var added;
+    vObj.properties = vObj.ref.bindProperties(remoteScope, {
+      onProperties: function(newProps) {
+        vObj.properties = newProps;
+        if(!added) {
+          if(callbacks.onAdd) {
+            callbacks.onAdd(vObj, vObj.ref, addNow);
+          }
+          else {
+            addNow(); //adds the object into the array
+          }
         } else {
-          var callbacks = callbacks || {}
-        }
-
-        props = {}
-        varName && (remoteScope[varName] = props)
-        var replaceObj = function(src, dest) {
-          for(var prop in src) {
-            delete src[prop]
+          if(callbacks.onChange) {
+            callbacks.onChange(vObj, vObj.ref, updateScope);
           }
-
-          for(var prop in dest) {
-            src[prop] = dest[prop]
+          else {
+            updateScope();// just update the scope, and dont call addNow() which adds a new obj
           }
         }
-
-        ref.on('properties',function(error, ref, vSnap) {
-          if(error) {
-            throw error
-            return
-          }
-          remoteScopeProps = remoteScope
-          onPropsUnbind = callbacks.onUnbind
-          var onProperties = callbacks.onProperties
-          var done = function(){
-            remoteScope.$apply()
-          }
-          replaceObj(props, vSnap.properties())
-          if(onProperties) {
-            onProperties(remoteScope, props, ref, done)
-          } else {
-            done()
-          }
-        })
-
-        remoteScope.$on('$destroy', function() {
-          unbindProperties()
-        })
-
-        return props
+      },
+      onUnbind: function() {
       }
+    });
+  }
 
-      var edgeRefs = {}
-      var edges = []
-      var callbacks
-      var bindSecondLevelEdges
-      var bindOutVertex
-      var secondLevelBinding
-      var remoteScope
-      var bindEdges = function(remoteS, varName, bindOutV, bindSecondLevelE, cb, reverse) {
-        remoteScope = remoteS
-        if(typeof varName === "boolean") {
-          var reverse = cb
-          callbacks = bindSecondLevelE || {}
-          var bindSecondLevelEdges = bindOutV
-          var bindOutVertex = varName
-          varName = undefined
-        } else {
-          callbacks = cb || {}
-          bindSecondLevelEdges = bindSecondLevelE
-          bindOutVertex = bindOutV
-        }
-        secondLevelBinding = bindOutVertex || bindSecondLevelEdges
+  handleRemove = function(name, vObj, removeNow) {
+    removeNow = $timeout.bind(null, removeNow);
+    vObj.ref.unbindProperties();
+    if(callbacks.onRemove) {
+      callbacks.onRemove(vObj, vObj.ref, removeNow);
+    } else {
+      removeNow();
+    }
+  };
 
-        var toBeDeleted = {}
-        varName && (remoteScope[varName] = edges)
-
-        var add = function(edgeSnap, edgeRef) {
-          var name = edgeSnap.name()
-          var priority = edgeSnap.priority()
-
-          if(toBeDeleted[name] > 0) {
-            toBeDeleted[name]--
-            return
-          }
-
-          edgeRefs[name] && console.log("Shouldn't happen.")
-          edgeRefs[name] = edgeRef
-
-          var added;
-          var edgeObj = { name:name, priority:priority }
-
-          var addEdgeToArray  = function () {
-            for(var i = 0; i<edges.length && !added; i++) {
-              if(reverse? edges[i].priority < priority : edges[i].priority < priority) {
-                edges.splice(i, 0, edgeObj)
-                added = true
-                break
-              }
-            }
-
-            if(!added) {
-              reverse? edges.unshift(edgeObj) : edges.push(edgeObj)
-              added = true
-            }
-
-            if(toBeDeleted[name] > 0) {
-              toBeDeleted[name]--
-              remove(edgeSnap)
-            }
-          }
-
-          edgeRef.on('properties',function(error, r, outVertexSnap) {
-            !bindOutVertex && edgeRef.off('properties')
-            if(!callbacks.onAdd || added) {
-              remoteScope.$apply(function() {
-                edgeObj.properties = outVertexSnap.properties()
-                if(!added) {
-                  addEdgeToArray()
-                }
-              })
-            } else {
-              edgeObj.properties = outVertexSnap.properties()
-            }
-
-            if(!added && callbacks.onAdd) {
-              callbacks.onAdd(remoteScope ,edgeObj, edgeRef, remoteScope.$apply.bind(remoteScope, addEdgeToArray))
-            } else if(added && callbacks.onChange) {
-              callbacks.onChange(remoteScope ,edgeObj, edgeRef, remoteScope.$apply.bind(remoteScope, addEdgeToArray))
-            }
-          })
-
-          if(bindSecondLevelEdges)
-            edgeObj.edges = ngAppbaseRef(edgeRef).$bindEdges(remoteScope, true)
-        }
-
-        var remove = function(edgeSnap) {
-          var name = edgeSnap.name()
-          var deleted;
-          for(var i = 0; i < edges.length; i++) {
-            if(edges[i].name === name) {
-              if(secondLevelBinding) {
-                edgeRefs[name].off()
-              }
-              var proceedDeletetion = function() {
-                $timeout(function() {
-                  for(var j = 0; j < edges.length; j++) {
-                    if(edges[j].name === name) {
-                      edges.splice(j ,1)
-                      break
-                    }
-                  }
-                })
-              }
-              if(callbacks.onRemove){
-                callbacks.onRemove(remoteScope, edges[i], edgeRefs[name], proceedDeletetion)
-              }
-              else
-                proceedDeletetion()
-
-              delete edgeRefs[name]
-
-              deleted = true
-              break
-            }
-          }
-          if(!deleted){
-            if(toBeDeleted[name] === undefined)
-              toBeDeleted[name] = 0
-
-            toBeDeleted[name]++
-          }
-        }
-
-        ref.on('edge_added',function(error,edgeRef,edgeSnap) {
-          if(error){
-            throw error
-            return
-          }
-          add(edgeSnap, edgeRef)
-        })
-
-        ref.on('edge_removed',function(error,edgeRef,edgeSnap) {
-          if(error){
-            throw error
-            return
-          }
-          remove(edgeSnap)
-        })
-
-        ref.on('edge_changed',function(error,edgeRef,edgeSnap) {
-          if(error) {
-            throw error
-            return
-          }
-          remove(edgeSnap)
-          add(edgeSnap, edgeRef)
-        })
-
-        remoteScope.$on('$destroy', function() {
-          unbindEdges()
-        })
-
-        return edges
-      }
-
-      var unbindProperties = function(){
-        ref.off('properties')
-        onPropsUnbind && onPropsUnbind(remoteScopeProps, props, ref)
-      }
-
-      var unbindEdges = function() {
-        ref.off('edge_added')
-        ref.off('edge_removed')
-        ref.off('edge_changed')
-        while(edges.length > 0) {
-          bindSecondLevelEdges && edgeRefs[edges[i].name].off()
-          var edgeObj = edges[0]
-          var edgeRef = edgeRefs[edges[0].name]
-          delete edgeRefs[edges[0].name]
-          edges.splice(0, 1)
-          callbacks.onUnbind && callbacks.onUnbind(remoteScope, edgeObj , edgeRef)
+  exports.add = function(name, obj) {
+    if(toBeDeleted[name] > 0) {
+      toBeDeleted[name]--
+      return
+    }
+    var added;
+    var addNow = function () {
+      for(var i = 0; i<exports.data.length && !added && obj.priority !== undefined; i++) {
+        if(reverse? exports.data[i].priority < obj.priority : exports.data[i].priority < obj.priority) {
+          exports.data.splice(i, 0, obj)
+          added = true
+          break
         }
       }
 
-      return {
-        $ref: function() {
-          return ref
-        },
-        $setData: ref.setData,
-        $removeData: ref.removeData,
-        $path: ref.path,
-        $URL: ref.URL,
-        $setEdge: function($abRef, name ,priority ,callback){
-          return ref.setEdge($abRef.$ref(), name, priority, callback)
-        },
-        $removeEdge: ref.removeEdge,
-        $inVertex: function() {
-          return ngAppbaseRef(ref.inVertex())
-        },
-        $outVertex: function(edgeName) {
-          return ngAppbaseRef(ref.outVertex(edgeName))
-        },
-        $bindProperties:bindProperties,
-        $bindEdges:bindEdges,
-        $unbindProperties: unbindProperties,
-        $unbindEdges: unbindEdges,
-        $unbind: function() {
-          unbindProperties()
-          unbindEdges()
+      if(!added) {
+        reverse? exports.data.unshift(obj) : exports.data.push(obj)
+        added = true
+      }
+
+      //checking if delete was called on the name while performing onAdd
+      if(toBeDeleted[name] > 0) {
+        toBeDeleted[name]--
+        exports.remove(name)
+      }
+    }
+
+    if(handleAdd) {
+      handleAdd(name, obj, addNow);
+    } else {
+      addNow();
+    }
+  }
+
+  exports.remove = function(name) {
+    var deleted;
+    var removeNow = function() {
+      for(var j = 0; j < exports.data.length; j++) {
+        if(exports.data[j].name === name) {
+          exports.data.splice(j ,1);
+          break;
         }
       }
     }
 
-    return ngAppbaseRef
+    for(var i = 0; i < exports.data.length; i++) {
+      if(exports.data[i].name === name) {
+        if(handleRemove) {
+          handleRemove(name, exports.data[i], removeNow);
+        } else {
+          removeNow();
+        }
+        deleted = true;
+        break;
+      }
+    }
+
+    if(!deleted) {
+      if(toBeDeleted[name] === undefined)
+        toBeDeleted[name] = 0;
+      toBeDeleted[name]++;
+    }
+  }
+
+  return exports;
+}
+
+angular.module('Appbase',[])
+  .factory('$appbase', function($timeout) {
+    if(typeof Appbase === 'undefined') {
+      throw ("Appbase library is not loaded.");
+    } else if (Appbase.ns === undefined) {
+      throw ("Wrong version of Appbase library is loaded. Make sure you are using v2.0");
+    }
+  
+    var $appbase = {};
+
+    //returns an Appbase namespace reference, with injected methods: bindVertices, unbindVertices, unbind
+    $appbase.ns = function(namespace) {
+      var nsRef = Appbase.ns(namespace);
+      var nsRefCopy = Appbase.ns(namespace); // as bind-unbind rely on 'on' methods, they are done on this ref, so that they dont interfere with 'on' methods of the refrence which is returned.
+      var origMethods = {};
+      var callbacks;
+      var remoteScope;
+      
+      nsRef.bindVertices = function(remoteS, cb, reverse) {
+        remoteScope = remoteS;
+        callbacks = cb || {};
+        dataExposed = phaadArray(reverse, callbacks, $timeout, remoteScope);
+        
+        nsRefCopy.on('vertex_added', function(error, vRef) {
+          if(error) {
+            throw error;
+            return;
+          }
+          //TODO: name and proper vRef
+          var vPath = parsePath(vRef.path()).v;
+          var vKey = parsePath(vPath).ns;
+          var vObj = { name: vKey, ref: vRefNG(vRef.path())};
+          dataExposed.add(vObj.name, vObj);
+        })
+
+        nsRefCopy.on('vertex_destroyed',function(error, vRef) {
+          if(error) {
+            throw error;
+            return;
+          }
+          var vPath = parsePath(vRef.path()).v;
+          var vKey = parsePath(vPath).ns;
+          dataExposed.remove(vKey);
+        })
+
+        remoteScope && remoteScope.$on('$destroy', function() {
+          ref.unbindEdges();
+        })
+
+        return dataExposed.data;
+      }
+
+      nsRef.unbindVertices = function() {
+        nsRefCopy.off('vertex_added');
+        nsRefCopy.off('vertex_destroyed');
+        while(dataExposed.data.length > 0) {
+          var vObj = dataExposed.data[0];
+          dataExposed.data.splice(0, 1);
+          vObj.ref.unbindProperties();
+          callbacks.onUnbind && callbacks.onUnbind(vObj);
+        }
+      }
+      
+      nsRef.unbind = nsRef.unbindVertices;
+      
+      nsRef.v = function(path) {
+        return vRefNG(namespace + '/' + path);
+      }
+
+      return nsRef;
+    }
+
+    //returns an Appbase vertex reference, with injected methods: bindProperties, bindEdges, unbindProperties, unbindEdges, unbind
+    var vRefNG = function(path) {
+      var ref = Appbase.ns(parsePath(path).ns).v(parsePath(path).v); //inject methods on this reference
+      var refCopy = Appbase.ns(parsePath(path).ns).v(parsePath(path).v);  // as bind-unbind rely on 'on' methods, they are done on this ref, so that they dont interfere with 'on' methods of the refrence which is returned.
+      
+      var callbacks = {
+        properties: {
+        }, 
+        edges: {
+        }
+      };
+      var remoteScopes = {};
+      var dataExposed = {
+        properties: {}
+      };
+
+      ref.bindProperties = function(remoteScope, cbs) {
+        remoteScopes.properties = remoteScope;
+        callbacks.properties = cbs || {};
+        refCopy.on('properties', function(error, r, vSnap) {
+          if(error) {
+            throw error;
+          }
+          
+          var newProps = vSnap.properties();
+          var done = $timeout.bind(null, replaceObjInPlace.bind(null, newProps, dataExposed.properties));
+          if(callbacks.properties.onProperties) {
+            callbacks.properties.onProperties(newProps, ref, done);
+          } else {
+            done();
+          }
+        });
+
+        remoteScope && remoteScope.$on('$destroy', function() {
+          ref.unbindProperties();
+        })
+
+        return dataExposed.properties;
+      }
+      
+      ref.unbindProperties = function() {
+        refCopy.off('properties');
+        callbacks.properties.onUnbind && callbacks.properties.onUnbind(dataExposed.properties, ref);
+      }
+
+      ref.bindEdges = function(remoteScope, cb, reverse) {
+        remoteScopes.edges = remoteScope;
+        callbacks.edges = cb || {};
+        dataExposed.edges = phaadArray(reverse, callbacks.edges, $timeout, remoteScope);
+        
+        refCopy.on('edge_added',function(error, edgeRef, edgeSnap) {
+          if(error) {
+            throw error;
+            return;
+          }
+          var vObj = { name: edgeSnap.name(), priority: edgeSnap.priority(), ref: vRefNG(edgeRef.path())};
+          dataExposed.edges.add(vObj.name, vObj);
+        })
+
+        refCopy.on('edge_removed',function(error, edgeRef, edgeSnap) {
+          if(error) {
+            throw error;
+            return;
+          }
+          dataExposed.edges.remove(edgeSnap.name());
+        })
+
+        refCopy.on('edge_changed',function(error, edgeRef, edgeSnap) {
+          if(error) {
+            throw error;
+            return;
+          }
+          
+          dataExposed.edges.remove(edgeSnap.name());
+          dataExposed.edges.add(edgeSnap.name(), {name: edgeSnap.name(), priority: edgeSnap.priority() , ref: vRefNG(edgeRef.path())});
+        })
+
+        remoteScope && remoteScope.$on('$destroy', function() {
+          ref.unbindEdges();
+        })
+
+        return dataExposed.edges.data;
+      }
+
+      ref.unbindEdges = function() {
+        refCopy.off('edge_added');
+        refCopy.off('edge_removed');
+        refCopy.off('edge_changed');
+        while(dataExposed.edges.data.length > 0) {
+          var vObj = dataExposed.edges.data[0];
+          dataExposed.edges.data.splice(0, 1);
+          vObj.ref.unbindProperties();
+          callbacks.edges.onUnbind && callbacks.edges.onUnbind(vObj);
+        }
+      }
+      
+      ref.unbind = function() {
+        ref.unbindEdges();
+        ref.unbindProperties();
+      }
+      
+      return ref;
+    }
+
+    for(var methodName in Appbase) {
+      if($appbase[methodName] === undefined) {
+        $appbase[methodName] = Appbase[methodName];
+      }
+    }
+    return $appbase;
   })
+
+}());
